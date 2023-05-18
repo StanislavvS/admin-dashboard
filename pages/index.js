5871ecff69421d824f5e282eb4f16d60a9ce00c4
Stanislaw Sobczak <stanislawsobczak@MacBook-Pro-Stanislaw.local>
2023-05-18 20:56:35 +0200

    Add login functionality

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 rounded-lg px-4">
-          Login with Google
-        </button>
+  const { data: session } = useSession();
+
+  if (!session)
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 rounded-lg px-4"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+
+  return <div>Logged in {session.user.email}</div>;
 }
