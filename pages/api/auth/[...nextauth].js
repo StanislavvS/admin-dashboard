3e223cd6cf27e773f491a5f81dc11350ea2fe979
Stanislaw Sobczak <stanislawsobczak@MacBook-Pro-Stanislaw.local>
2023-05-19 19:01:45 +0200

    Setup monogdb connectiona

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
 
 export const authOptions = {
   providers: [
@@ -8,6 +10,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export default NextAuth(authOptions);
