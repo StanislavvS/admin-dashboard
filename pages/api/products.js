b467104594169853256534679dc21868547eed16
Stanislaw Sobczak <stanislawsobczak@MacBook-Pro-Stanislaw.local>
2023-05-22 18:17:36 +0200

    Add product model

---
+++ pages/api/products.js
@@ -0,0 +1,14 @@
+import mongooseConnect from "@/lib/mongooose";
+import Product from "@/models/Product";
+
+export default async function hanlder(req, res) {
+  const { method } = req;
+
+  if (method === "POST") {
+    await mongooseConnect();
+    const { title, description, price } = req.body;
+    const productDoc = await Product.create({ title, description, price });
+
+    res.json(productDoc);
+  }
+}
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,18 +1,46 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
 
 const NewProduct = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const createProduct = async (e) => {
+    e.preventDefault();
+    const data = { title, description, price };
+    axios.post("/api/products", data);
+  };
+
   return (
     <Layout>
-      <h1>New Product</h1>
-      <label>Product name</label>
-      <input type="text" placeholder="product name" />
-      <label>Description</label>
-      <textarea placeholder="description" />
-      <label>Price (in USD)</label>
-      <input type="number" placeholder="price" />
-      <button className="btn-primary" type="submit">
-        Save
-      </button>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label>Product name</label>
+        <input
+          type="text"
+          placeholder="product name"
+          value={title}
+          onChange={(e) => setTitle(e.currentTarget.value)}
+        />
+        <label>Description</label>
+        <textarea
+          placeholder="description"
+          value={description}
+          onChange={(e) => setDescription(e.currentTarget.value)}
+        />
+        <label>Price (in USD)</label>
+        <input
+          type="number"
+          placeholder="price"
+          value={price}
+          onChange={(e) => setPrice(e.currentTarget.value)}
+        />
+        <button className="btn-primary" type="submit">
+          Save
+        </button>
+      </form>
     </Layout>
   );
 };
