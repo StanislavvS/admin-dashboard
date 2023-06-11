6a0fbd3ef648bd73ba0d21d5d1d53b81acf002cc
Stanislaw Sobczak <sstanislaw85@gmail.com>
2023-06-11 17:53:17 +0200

    Add upload api route

--- components/ProductsForm.js
+++ components/ProductsForm.js
@@ -7,6 +7,7 @@ const ProductForm = ({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) => {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -30,6 +31,22 @@ const ProductForm = ({
     router.push("/products");
   }
 
+  const uploadImages = async (e) => {
+    const files = e.target?.files;
+
+    if (files) {
+      const formData = new FormData();
+
+      for (const file of files) {
+        formData.append("file", file);
+      }
+
+      const res = await axios.post("/api/upload", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    }
+  };
+
   return (
     <form onSubmit={saveProduct}>
       <h1>New Product</h1>
@@ -40,6 +57,28 @@ const ProductForm = ({
         value={title}
         onChange={(e) => setTitle(e.currentTarget.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24  text-center flex items-center cursor-pointer justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" className="hidden" onChange={uploadImages}></input>
+        </label>
+        {!images?.length && <div>No photos in this product.</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
