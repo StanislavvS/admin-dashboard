8ee9b54bde564aa0cf854554cda500581cf7e9bd
Stanislaw Sobczak <sstanislaw85@gmail.com>
2023-06-14 05:36:13 +0200

    Create and fetch categories

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -3,16 +3,15 @@ import Category from "@/models/Category";
 
 export default async function handle(req, res) {
   const { method } = req;
+  const { name, parentCategory } = req.body;
   await mongooseConnect();
 
   if (method === "GET") {
-    res.json(await Category.find());
+    res.json(await Category.find().populate("parent"));
   }
 
   if (method === "POST") {
-    const { name } = req.body;
-
-    const categoryDoc = await Category.create({ name });
+    const categoryDoc = await Category.create({ name, parent: parentCategory });
     res.json(categoryDoc);
   }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -5,19 +5,23 @@ import { useState, useEffect } from "react";
 const Categories = () => {
   const [name, setName] = useState("");
   const [categories, setCategories] = useState([]);
-
-  console.log(categories);
+  const [parentCategory, setParentCategory] = useState("");
 
   useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = () => {
     axios.get(`/api/categories/`).then((result) => {
       setCategories(result.data);
     });
-  }, []);
+  };
 
   const saveCategory = async (e) => {
     e.preventDefault();
-    await axios.post(`/api/categories`, { name });
+    await axios.post(`/api/categories`, { name, parentCategory });
     setName("");
+    fetchCategories();
   };
 
   return (
@@ -33,6 +37,19 @@ const Categories = () => {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        <select
+          className="mb-0"
+          onChange={(e) => setParentCategory(e.target.value)}
+        >
+          {" "}
+          <option value={0}> No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+        </select>
         <button type="submit" className="btn-primary py-1">
           Save
         </button>
@@ -41,6 +58,7 @@ const Categories = () => {
         <thead>
           <tr>
             <td>Category name</td>
+            <td>Parent category</td>
           </tr>
         </thead>
         <tbody>
@@ -48,6 +66,7 @@ const Categories = () => {
             categories.map((category, index) => (
               <tr key={index}>
                 <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
               </tr>
             ))}
         </tbody>
